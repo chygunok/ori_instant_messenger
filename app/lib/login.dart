0ef1023755e5ac95a82939e220867b132cda2c11
Dragonite24 <qwez-9@mail.ru>
2020-10-22 12:53:09 +0300

    Build and more
    Add LoginPage, mainMenu(empty page), add AppColors

---
+++ app/lib/login.dart
@@ -0,0 +1,75 @@
+import 'package:app/mainMenu.dart';
+import 'package:flutter/material.dart';
+import 'package:app/theme.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  TextEditingController controller = new TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    var login = TextField(
+      decoration: InputDecoration(
+        border: OutlineInputBorder(),
+        labelText: 'Логин',
+      ),
+      controller: controller,
+    );
+
+    return Scaffold(
+      body: Container(
+        color: AppColors.mainBlack[800],
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "ORI Мессенджер",
+                style: TextStyle(color: AppColors.mainBlack[200], fontSize: 48),
+              ),
+              Padding(
+                padding: EdgeInsets.all(16),
+                child: Container(width: 500, child: login),
+              ),
+              Container(
+                width: 500,
+                child: TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Пароль',
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(16),
+                child: MaterialButton(
+                  child: Text(
+                    "Войти",
+                    style: TextStyle(
+                      color: AppColors.mainBlack[800],
+                    ),
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (BuildContext context) => MainMenu(),
+                      ),
+                    );
+                  },
+                  minWidth: 500,
+                  height: 50,
+                  color: AppColors.mainBlack[200],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ app/lib/theme.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+// import 'package:font/fonts.dart';
+
+class AppColors {
+  static const mainBlack = MaterialColor(
+    0xFF202020,
+    {
+      900: Color(0xFF000000),
+      800: Color(0xFF202020),
+      700: Color(0xFF414141),
+      600: Color(0xFF616161),
+      500: Color(0xFF818181),
+      400: Color(0xFFA1A1A1),
+      300: Color(0xFFC1C1C1),
+      200: Color(0xFFE1E1E1),
+      100: Color(0xFFFFFFFF),
+    },
+  );
+
+  static const red = MaterialColor(
+    0xFFD8393F,
+    {
+      900: Color(0xFFbb292c),
+      800: Color(0xFFcb3337),
+      700: Color(0xFFd8393e),
+      600: Color(0xFFea4245),
+      500: Color(0xFFf94a46),
+      400: Color(0xFFf35b5e),
+      300: Color(0xFFe7797e),
+      200: Color(0xFFf09ea2),
+      100: Color(0xFFffd0d7),
+      50: Color(0xFFffecf0),
+    },
+  );
+}
+
+// class AppStyles {
+//   static final cardHeader = GoogleFonts.montserrat(
+//     fontSize: 18,
+//     fontWeight: FontWeight.w500,
+//   ) as TextStyle;
+
+//   static final pageHeader = GoogleFonts.montserrat(
+//     fontSize: 24,
+//     fontWeight: FontWeight.w500,
+//   ) as TextStyle;
+// }
