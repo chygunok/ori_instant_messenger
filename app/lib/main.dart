9acbf218542992582aca3e32ed7dd0d35ed61a51
Dragonite24 <qwez-9@mail.ru>
2020-11-02 12:25:28 +0300

    Update main.dart

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'ORI',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(scaffoldBackgroundColor: Colors.white),
       home: LoginPage(),
     );
