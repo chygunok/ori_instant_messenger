2857fc0223dc77006fd244a5d098cc4293f45e51
chygunok <73240721+chygunok@users.noreply.github.com>
2020-11-13 18:54:37 +0300

    Update mainMenu.dart

--- app/lib/mainMenu.dart
+++ app/lib/mainMenu.dart
@@ -1,14 +1,24 @@
-import 'package:app/login.dart';
 import 'package:app/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:app/recentChats.dart';
 
-class MainMenu extends StatelessWidget {
+// Ограничения для виджетов объявляются в родителях.
+// Размеры (желаемые) задаются в самом виджете.
+// Позиция виджета на экране устанавливается родителем
+
+class MainMenu extends StatefulWidget {
+  @override
+  _MainMenu createState() => _MainMenu();
+}
+
+class _MainMenu extends State<MainMenu> {
   @override
   Widget build(BuildContext context) {
-    Orientation deviceOrientation = MediaQuery.of(context).orientation;
-    Size screenSize = MediaQuery.of(context).size;
+    // Orientation deviceOrientation = MediaQuery.of(context).orientation;
+    // Size screenSize = MediaQuery.of(context).size;
     return Scaffold(
       appBar: MainAppBar(context),
+      // Hamburger
       drawer: Drawer(
         child: new ListView(
           children: <Widget>[
@@ -24,56 +34,54 @@ class MainMenu extends StatelessWidget {
             new ListTile(title: new Text("Профиль"), onTap: () {}),
             new ListTile(title: new Text("Сообщения"), onTap: () {}),
             new ListTile(title: new Text("Лента"), onTap: () {}),
-            new ListTile(title: new Text("Настройки"), onTap: () {}),
-            new ListTile(
-              title: new Text("Выйти"),
-              onTap: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => LoginPage()));
-              },
-            )
+            new ListTile(title: new Text("Настройки"), onTap: () {})
           ],
         ),
       ),
       body: Row(
+        // Разметка под списов чатов и сами статичные чаты RecentChats
         children: [
           Expanded(
             flex: 2,
             child: LayoutBuilder(
               builder: (context, constraints) => Container(
-                color: Colors.red,
-                child: Center(
-                  child: Text(
-                    'Поставьте',
-                    style: TextStyle(color: Colors.white, fontSize: 18),
-                  ),
+                color: Colors.black,
+                child: Column(
+                  children: <Widget>[
+                    Expanded(
+                      child: Container(
+                        child: Column(
+                          children: <Widget>[
+                            RecentChats(),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ),
           ),
           Expanded(
-            flex: 5,
+            flex: 4,
             child: Column(
               children: [
+                // Тут хезе что будет, скорее всего имя человека с которым ведется диалог
                 Expanded(
                   flex: 2,
-                  child: Container(
-                    color: Colors.red,
-                    child: OrientationBuilder(
-                      builder: (context, orientation) => Center(
-                        // child: Text(
-                        //   'View 1\n\n' +
-                        //       '[MediaQuery orientation]:\n$deviceOrientation\n\n' +
-                        //       '[OrientationBuilder]:\n$orientation',
-                        //   style: TextStyle(color: Colors.white, fontSize: 18),
-                        // ),
-                        child: Text('зачет',
-                            style:
-                                TextStyle(color: Colors.white, fontSize: 18)),
+                  child: OrientationBuilder(
+                    builder: (context, orientation) => Container(
+                      color: Colors.red,
+                      child: Center(
+                        child: Text(
+                          'пожалуйста',
+                          style: TextStyle(color: Colors.black, fontSize: 18),
+                        ),
                       ),
                     ),
                   ),
                 ),
+                // Вот тут должны быть сообщения с другим пользователем
                 Expanded(
                   flex: 3,
                   child: OrientationBuilder(
